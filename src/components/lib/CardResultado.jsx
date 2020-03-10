c8f48508130a3081364f888e34a2677aa71d44a0
Mauricio Biott <mauro@biott.com.ar>
2020-03-10 10:34:25 -0300

    cambios graficos sobre el card

--- src/components/lib/CardResultado.jsx
+++ src/components/lib/CardResultado.jsx
@@ -13,7 +13,9 @@ class CardResultado extends React.Component {
             <div className="CardResultado-container">
                 <div className="CardResultado-container-left">
                     <img src={this.props.data.imagen} alt="" />
-                    <div>3</div>
+                    <div className="CardResultado-container-left-detail">
+                        <h5>{this.props.data.name}</h5>
+                    </div>
                 </div>
                 <div className="CardResultado-container-right">
                     Derecha
