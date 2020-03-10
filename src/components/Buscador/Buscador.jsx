673a3daa074110c554d45149580a62d955c43511
Mauricio Biott <mauro@biott.com.ar>
2020-03-09 23:50:31 -0300

    detalles logo en navbar

--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -7,6 +7,7 @@ import './Buscador.css';
 import DateRangePicker from '../lib/DatePickerRange';
 import Autocomplete from '../lib/Autocomplete';
 import SelectTypes from '../lib/SelectTypes';
+import logo from '../../images/Trivago.svg';
 
 class Buscador extends React.Component {
 
@@ -14,9 +15,9 @@ class Buscador extends React.Component {
         super(props);
         this.state = {
             buscando: false,
-            autocomplete:"Sin valor",
-            dates:"Sin valor",
-            type:"Sin valor"
+            autocomplete: "Sin valor",
+            dates: "Sin valor",
+            type: "Sin valor"
         }
     }
 
@@ -27,18 +28,19 @@ class Buscador extends React.Component {
     render() {
         return (
             <div className="buscador_container" >
+
                 <div className="Buscador-autocomplete">
-                    <Autocomplete onSelect={(val) => this.setState({autocomplete:val})} />
+                    <Autocomplete onSelect={(val) => this.setState({ autocomplete: val })} />
                 </div>
                 <div className="Buscador-dates">
-                    <DateRangePicker  onSelect={(val) => this.setState({dates:val})} />
+                    <DateRangePicker onSelect={(val) => this.setState({ dates: val })} />
                 </div>
                 <div className="Buscador-types">
-                    <SelectTypes  onSelect={(val) => this.setState({type:val})} />
+                    <SelectTypes onSelect={(val) => this.setState({ type: val })} />
                 </div>
                 <div className="Buscador-button">
-                    <a href={'/buscador/' + this.state.autocomplete+'/'+this.state.dates+'/'+this.state.type}   className="btn btn-primary" 
-                    onClick={this.handleClick}>Buscar</a>
+                    <a href={'/buscador/' + this.state.autocomplete + '/' + this.state.dates + '/' + this.state.type} className="btn btn-primary"
+                        onClick={this.handleClick}>Buscar</a>
 
                 </div>
             </div>
--- src/pages/BuscadorScreen.js
+++ src/pages/BuscadorScreen.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Buscador from '../components/Buscador/Buscador';
 
 function BuscadorScreen(props) {
     console.log(props.match.params);
-  return (
-      <content>
-        <div ><br/><br/><br/>Tranbajando en la busqueda de resultados......
-        <br/>
-        <br/>
-        <b>Palabra clave:</b>{props.match.params.res}
-        <br/>
-        <b>Entre fechas:</b>{props.match.params.dates}
-        <br/>
-        <b>tipo:</b>{props.match.params.type}
-        </div>
+    return (
+        <content>
+            <div className="App-background-result"> 
+            <Buscador />
 
-      </content>
-  );
+            <div ><br /><br /><br />Tranbajando en la busqueda de resultados......
+        <br />
+                <br />
+                <b>Palabra clave:</b>{props.match.params.res}
+                <br />
+                <b>Entre fechas:</b>{props.match.params.dates}
+                <br />
+                <b>tipo:</b>{props.match.params.type}
+            </div>
+            </div>
+        </content>
+    );
 }
 
 export default BuscadorScreen;
