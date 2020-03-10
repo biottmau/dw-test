5ad6459d785bb67aa83f1ee670a200e3c17e3397
Mauricio Biott <mauro@biott.com.ar>
2020-03-10 10:22:09 -0300

    Preaparando el muestreo de resultado

--- src/Resultados/Resultados.jsx
+++ src/Resultados/Resultados.jsx
@@ -1,10 +1,35 @@
+/**
+ * Componente para motrar la lista de resultados
+ * Aca deberíamos trabajar paginación y demas
+ */
+
 import * as React from 'react';
+import BuscadorService from '../services/BuscadorService';
+import CardResultado from '../components/lib/CardResultado';
 
 
 class Resultados extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            data:[]
+        };
+    }
+    componentDidMount(){
+        BuscadorService.getResultados().then((data) => {
+            this.setState({data:data});
+        });
+    }
+
     render(){
+        let results = this.state.data;
         return (
-            <div>Resultados</div>
+            <div>
+            {results.map((d,i) => (
+                <CardResultado key={i} data={d} />
+            ))}
+            </div>
         );
     }
 }
---
+++ src/components/lib/CardResultado.jsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import './CardResultado.css';
+
+
+class CardResultado extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="CardResultado-container">
+                <div className="CardResultado-container-left">
+                    <img src={this.props.data.imagen} alt="" />
+                    <div>3</div>
+                </div>
+                <div className="CardResultado-container-right">
+                    Derecha
+                </div>
+                
+            </div>
+        );
+    }
+
+}
+
+export default CardResultado;
